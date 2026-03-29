649f85550e94173a1282523163cc628e91bf17a1
Olejeck <oleg.navrotskiy082007@gmail.com>
2026-03-29 23:26:42 +0300

    Bug fixes

--- src/main/java/com/ai_project/dataart/DataartApplication.java
+++ src/main/java/com/ai_project/dataart/DataartApplication.java
@@ -1,13 +1,20 @@
 package com.ai_project.dataart;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class DataartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataartApplication.class, args);
-	}
+    @PostConstruct
+    public void init() {
+        // Примусово встановлюємо UTC для всього додатка
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(DataartApplication.class, args);
+    }
 }
